6f49b9abbdd504ad551373bb45c2c7a261ce5084
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-25 23:52:54 -0700

    add text color for those items with an error class & add background color to every other li

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ const myList = document.getElementsByTagName('li');
 for (let i = 0; i < myList.length; i++) {
   myList[i].style.color = 'purple';
 }
+
+const errorList = document.getElementsByClassName("error-not-purple");
+
+for (let i = 0; i < errorList.length; i++) {
+  errorList[i].style.color = 'red';
+}
+
+const evens = document.querySelectorAll("li:nth-child(odd)");
+
+for (let i = 0; i < evens.length; i++) {
+  evens[i].style.backgroundColor = 'lightblue';
+}
