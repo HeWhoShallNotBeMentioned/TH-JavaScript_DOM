b7a606850282bcf3e9de97429e83b4969eae7d0a
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-25 22:47:45 -0700

    Grab all line elements to change text color

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ myButton.addEventListener('click', () => {
   let myColor = myTextInput.value;
   myHeading.style.color = myColor;
 });
+
+const myList = document.getElementsByTagName('li');
+
+for (let i = 0; i < myList.length; i++) {
+  myList[i].style.color = 'purple';
+}
